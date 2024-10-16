2b57a4c224e01e01c2e1a3f02edbee22770bdd0b
Ignacio Norambuena <inorambuenaa@gmail.com>
2024-10-16 14:47:17 -0300

    Config2

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   site: "https://archidemus.me",
+  base: "",
   integrations: [
     react(),
     tailwind({
